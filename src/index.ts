ca949c0377919f953d80af64353b62499593cf02
Gwamaka Mwamwaja <gmwamwaja@outlook.com>
2022-03-03 11:30:55 +0300

    set an entry point

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { parseInput, printSolution } from "./io";
 import { matrixMinDistanceToTarget } from "./solver";
 
+// Read the input file and solve the problems.
 const problems = parseInput("./input.txt");
 for (const problem of problems) {
   const solution = matrixMinDistanceToTarget(problem);
